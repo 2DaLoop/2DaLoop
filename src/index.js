74ea41c742df6dee1a43cf59dfd14f1204a3cf64
Emma Long <emmag2623@gmail.com>
2025-06-11 00:51:42 -0400

    testing search location and map markers

---
+++ src/index.js
@@ -0,0 +1,165 @@
+const { Map } = await google.maps.importLibrary("maps");
+const { Place } = await google.maps.importLibrary("places");
+const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+const { LatLngBounds } = await google.maps.importLibrary("core");
+const { Geocoder } = await google.maps.importLibrary("geocoding");
+
+const centerPosition = { lat: 39.8283, lng: -98.5795 }; // Geographic center of continental U.S.
+let map;
+
+initMap();
+
+// search a location, can't be too broad
+document.getElementById("btnSearch").addEventListener("click", async () => {
+    const locationText = document.getElementById("txtSearch").value;
+    if (locationText.trim() !== "") {
+        const geocoder = new Geocoder();
+
+        geocoder.geocode({ address: locationText }, async (results, status) => {
+            if (status === "OK" && results[0]) {
+                const location = results[0].geometry.location;
+                const latLng = {
+                    lat: location.lat(),
+                    lng: location.lng(),
+                };
+                console.log(latLng);
+
+                await searchLocation(latLng);
+            } else {
+                alert("Geocoding failed: " + status);
+            }
+        });
+    }
+});
+
+// search based on current location
+document
+    .getElementById("btnCurrLocation")
+    .addEventListener("click", async () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                async function (location) {
+                    const userLocation = {
+                        lat: location.coords.latitude,
+                        lng: location.coords.longitude,
+                    };
+
+                    await searchLocation(userLocation);
+                },
+                function (error) {
+                    strError = "";
+
+                    // alert user to enable location sharing if denied or other error
+                    if (error.code === error.PERMISSION_DENIED) {
+                        strError =
+                            "Please enable location sharing in your settings!";
+                    } else {
+                        strError = "There was an issue getting your location!";
+                    }
+                    alert(strError);
+                }
+            );
+        }
+    });
+
+// initialize map centered on the U.S.
+async function initMap() {
+    map = new Map(document.getElementById("map"), {
+        center: centerPosition,
+        zoom: 4,
+        mapId: "e8f578253d8c676318b940c1-",
+    });
+}
+
+// load GeoJSON data
+// can't use google maps api to filter this data based on location and radius, have to do manually
+async function initGeoJson() {
+    map.data.loadGeoJson("src/assets/geojson/MRFs.geojson");
+    map.data.loadGeoJson("src/assets/geojson/ElectronicsRecyclers.geojson");
+
+    // Optional styling for GeoJSON features
+    map.data.setStyle({
+        icon: {
+            url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+        },
+    });
+}
+
+// searches "electronics_stores" within 10km of given location
+async function searchLocation(location) {
+    const request = {
+        fields: ["displayName", "location", "businessStatus"],
+        locationRestriction: {
+            center: location,
+            radius: 10000,
+        },
+        includedTypes: ["electronics_store"],
+        language: "en-US",
+        region: "us",
+    };
+
+    const { places } = await Place.searchNearby(request);
+
+    if (places.length) {
+        console.log("Found places:", places);
+
+        const bounds = new LatLngBounds();
+        places.forEach((place) => {
+            new AdvancedMarkerElement({
+                map,
+                position: place.location,
+                title: place.displayName,
+            });
+            bounds.extend(place.location);
+        });
+
+        map.fitBounds(bounds);
+    } else {
+        console.log("No results for:", location);
+    }
+}
+
+// example of textSearch method
+async function performTextSearch(query, includedType) {
+    const request = {
+        textQuery: query,
+        fields: ["displayName", "location", "businessStatus"],
+        includedType,
+        language: "en-US",
+        region: "us",
+        useStrictTypeFiltering: false,
+    };
+
+    const { places } = await Place.searchByText(request);
+
+    if (places.length) {
+        console.log("Found places:", places);
+
+        const bounds = new LatLngBounds();
+        places.forEach((place) => {
+            new AdvancedMarkerElement({
+                map,
+                position: place.location,
+                title: place.displayName,
+            });
+            bounds.extend(place.location);
+        });
+
+        map.fitBounds(bounds);
+    } else {
+        console.log("No results found for:", query);
+    }
+}
+
+/*
+NOTES:
+
+    Either figure out how to search all the facilities we need to show on the map
+    or use GeoJSON data and manually filter.
+    Or both??
+    For first option, we'd have to use the textSearch method for all places that might be relevant. 
+
+    Instead of using geocoding to search locations, we could use the Places Search Box. It would
+    auto fill for users and more user friendly.
+    https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+*/
