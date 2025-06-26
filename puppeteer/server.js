4e2a368d3608d05bafea2681ca9d19bce7294e8d
Emma Long <emmag2623@gmail.com>
2025-06-26 00:31:08 -0400

    change to import

--- puppeteer/server.js
+++ puppeteer/server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const puppeteer = require("puppeteer");
+import express from 'express';
+import puppeteer from 'puppeteer';
+
 const app = express();
 const HTTP_PORT = 3000;
 
