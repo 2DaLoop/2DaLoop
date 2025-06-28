2d360148e44d6bed6b346b85649e0d00aca1412d
Emma Long <emmag2623@gmail.com>
2025-06-27 21:12:21 -0400

    testing puppeteer, returns results

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = "https://ttswhygqrukmssbcpjps.supabase.co";
+const SUPABASE_URL = window.ENV.SUPABASE_URL;
 const SUPABASE_ANON_KEY = window.ENV.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
