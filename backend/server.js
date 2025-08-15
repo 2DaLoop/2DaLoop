7c47d448ff0c5b0b6c8cafcad534020d93b2f684
Emma Long <emmag2623@gmail.com>
2025-08-14 21:33:35 -0400

    github deploy test

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import puppeteer from 'puppeteer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const HTTP_PORT = 3000;
 
+// Serve static files from "src"
+app.use(express.static(path.join(__dirname, '..')));
 app.use(cors());
 app.use(express.json());
 
