19944a4f16050ad07eeed4cdd01f301e9e90dc4d
MeccaCAO <mortiz@meccavation.com>
2025-09-11 21:27:04 -0400

    Final Version Launch

---
+++ 2DaLoop-dev/2DaLoop-dev/backend/server.js
@@ -0,0 +1,218 @@
+import express from 'express';
+import cors from 'cors';
+import puppeteer from 'puppeteer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const HTTP_PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+app.post("/calculate/budget", async (req, res) => {
+    try {
+        const { inputValues } = req.body;
+        if (!inputValues) {
+            return res.status(400).json({ error: "Input values are required" });
+        }
+
+        const inputElements = [
+            "desktop_pc_quantity",
+            "laptop_pc_quantity",
+            "network_device_quantity",
+            "telecom_quantity",
+            "server_quantity",
+            "desktop_pc_age",
+            "laptop_pc_age",
+            "network_device_age",
+            "telecom_age",
+            "server_age",
+        ];
+
+        const resultElements = [
+            "total_pickup_cost",
+            "total_service_fees",
+            "total_value_recovery",
+            "net_financial_settlement",
+        ];
+
+        // go to itad calculator
+        const browser = await puppeteer.launch({
+            headless: 'true',
+            args: [
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+            ],
+        });
+        const page = await browser.newPage();
+        await page.goto("https://calculator.itadusa.com/");
+
+        // input values in all fields
+        for (const input of inputElements) {
+            const element = await page.$(`#${input}`);
+            await element.click({ clickCount: 3 });
+            await element.type(inputValues[input].toString());
+        }
+
+        // click submit
+        await page.click("#button_submit");
+
+        // result object for both
+        const resultValues = {
+            noPacking: {},
+            packing: {},
+        };
+
+        // get results for no packing services
+        await sleep(1000);
+        for (const result of resultElements) {
+            const element = await page.$(`#${result}`);
+            const resultText = await page.evaluate((el) => el.value, element);
+            resultValues.noPacking[result] = resultText;
+        }
+
+        // toggle button for packing services
+        await page.click('#btn_service');
+        await page.click("#button_submit");
+
+        // get results after submission
+        await sleep(1000);
+        for (const result of resultElements) {
+            const element = await page.$(`#${result}`);
+            const resultText = await page.evaluate((el) => el.value, element);
+            resultValues.packing[result] = resultText;
+        }
+
+        // return results
+        res.status(200).json({ data: resultValues });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Something went wrong"+error,
+        });
+    }
+});
+
+app.post("/calculate/ghg", async (req, res) => {
+    try {
+        const { inputValues } = req.body;
+        if (!inputValues) {
+            return res.status(400).json({ error: "Input values are required" });
+        }
+
+        const inputPlaceholders = [
+            "Laptops",
+            "Servers",
+            "Desktops",
+            "Battery",
+            "Mobile",
+            "Printers",
+            "Storage",
+            "Monitors"
+        ];
+
+        const resultLabels = [
+            "ghg_emissions",
+            "powering_houses",
+            "removing_cars",
+            "solid_waste",
+            "air_emissions",
+            "water_emissions",
+            "gold",
+            "platinum",
+            "palladium",
+            "steel",
+            "copper",
+            "aluminium"
+        ];
+
+        const browser = await puppeteer.launch({
+            headless: 'true',
+            args: [
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+            ],
+        });
+        const page = await browser.newPage();
+        await page.goto("https://www.greenteksolutionsllc.com/itad-environmental-impact-calculator/", {
+            waitUntil: 'networkidle2'
+        });
+
+        // mouse needs to hover on site to work
+        await sleep(1000);
+        await page.mouse.move(100, 100);
+
+        // click 'calculate by num of assets' btn
+        await page.click('xpath=//button[contains(., "Calculate By Number Of Assets")]');
+
+        await sleep(1000);
+
+        // fill in input fields
+        for (const input of inputPlaceholders) {
+            // account for fields with multiple words
+            let fullInput;
+            if (input == "Battery") {
+                fullInput = "Battery Backups";
+            } else if (input == "Mobile") {
+                fullInput = "Mobile Devices";
+            } else if (input == "Storage") {
+                fullInput = "Storage Arrays";
+            } else {
+                fullInput = input;
+            }
+
+            // input values
+            await page.type(`xpath=//input[@placeholder="# of ${fullInput}"]`, inputValues[input].toString());
+        }
+
+        // click 'calculate' btn
+        await page.click('xpath=//button[contains(@class, "secondary-button") and contains(., "Calculate")]');
+
+        // get results
+        await sleep(1500);
+        const results = await page.$$eval('span.number', (spans, labels) => {
+            const data = {};
+
+            spans.forEach((span, index) => {
+                const numberText = span.textContent.trim();
+                const label = labels[index] || `unknown_${index}`;
+                data[label] = numberText;
+            });
+
+            const totalEstWeight = document.querySelector('span.font-bold');
+            if (totalEstWeight) {
+                data['total_est_weight'] = totalEstWeight.textContent.split(" ")[0];
+            }
+
+            return data;
+        }, resultLabels);
+
+        res.status(200).json({ data: results });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Something went wrong",
+        });
+    }    
+})
+
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on", HTTP_PORT);
+});
--- src/supabase/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = window.ENV.SUPABASE_URL;
-const SUPABASE_ANON_KEY = window.ENV.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
